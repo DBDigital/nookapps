722eae4885b4bf415156be91ec3a61a063879873
kbsriram <kbsriram@2bb34892-f56b-11de-9a6a-9d1f184cc70b>
2010-02-04 16:28:44 +0000

    Start splitting out the parsing code, and obtain power-settings from system settings rather than a hard-coded constant.

--- trook/src/com/kbs/util/NookUtils.java
+++ trook/src/com/kbs/util/NookUtils.java
@@ -2,6 +2,9 @@ package com.kbs.util;
 
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
+import android.provider.Settings;
+import android.database.Cursor;
 
 // A collection of random, nook-specific
 // stuff
@@ -18,12 +21,58 @@ public class NookUtils
     public final static String STATUSBAR_ACTION =
         "Statusbar.action";
 
+    public final static long DEFAULT_SCREENSAVER_DELAY = 300*1000L;
+
+    private final static String TAG = "nook-utils";
+
     public final static void setAppTitle(Context ctx, String title)
     {
         Intent msg = new Intent(UPDATE_TITLE);
 
         msg.putExtra("apptitle", title);
         ctx.sendBroadcast(msg);
+    }
+
+    public final static long getScreenSaverDelay(Context ctx)
+    {
+        String[] values = {
+            "value"
+        };
+
+        String[] fname = {
+            "bnScreensaverDelay"
+        };
+
+        long ret = DEFAULT_SCREENSAVER_DELAY;
+
+        Cursor c = null;
+
+        try {
+
+            c = ctx.getContentResolver().query
+                (Settings.System.CONTENT_URI, values, "name=?", fname, null);
+            if (c != null) {
+                c.moveToFirst();
+                long l = c.getLong(0);
+                if (l > 0) {
+                    ret = l;
+                    Log.d(TAG, "Found screen-saver delay as "+ret);
+                }
+            }
+            else {
+                Log.d(TAG, "No screen-saver-delay");
+            }
+        }
+        catch (Throwable th) {
+            Log.d(TAG, "Ignoring error when reading system settings", th);
+        }
+        finally {
+            if (c != null) {
+                try { c.close(); } catch (Throwable ign) {}
+                try {c.deactivate(); } catch (Throwable ign) {}
+            }
+        }
+        return ret;
     }
 
     public final static void showPageNumber
@@ -36,4 +85,5 @@ public class NookUtils
         msg.putExtra("max", maxpage);
         ctx.sendBroadcast(msg);
     }
+
 }
