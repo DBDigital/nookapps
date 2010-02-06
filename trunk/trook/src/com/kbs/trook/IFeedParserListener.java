1ec2d5eb69e518847d680bad26339d8c5dfca6c4
kbsriram <kbsriram@2bb34892-f56b-11de-9a6a-9d1f184cc70b>
2010-02-06 22:31:59 +0000

    Add some simple tests to check feeds, include trivial RDF parsing, and always serve RSS out of cache when possible.

---
+++ trunk/trook/src/com/kbs/trook/IFeedParserListener.java
@@ -0,0 +1,28 @@
+package com.kbs.trook;
+
+// A generic interface that allows an IFeedParser
+// to notify it of stuff that happens during a
+// parse.
+
+import java.io.IOException;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParser;
+
+public interface IFeedParserListener
+{
+    public FeedInfo getFeedInfo();
+
+    public String getStringResource(int v);
+
+    public void setResolvePath(String s);
+
+    public String getResolvePath();
+
+    public void setOpenSearchUrl(String s);
+
+    public void setStanzaSearchUrl(String s);
+
+    public void publishProgress1(FeedInfo.EntryInfo... ei);
+
+    public String fix(String s);
+}
